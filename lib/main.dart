15db8531de369139055482b0d8ce04628dfc6b2f
glayson <glayson.visgueira@paraiba.email>
2022-08-17 14:21:24 -0300

    Add material widget appbar e action button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,15 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(Text('Bem vindo', textDirection: TextDirection.ltr,));
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+      ),
+    ));
